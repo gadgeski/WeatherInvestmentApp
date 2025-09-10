f9441219cf657ee5d50a820a91dea156a238594e
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-10 13:05:21 +0900

    feat: (WeatherInvestmentAppApp.swift)

---
+++ WeatherInvestmentApp/WeatherInvestmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInvestmentAppApp.swift
+//  WeatherInvestmentApp
+//
+//  Created by Dev Tech on 2025/09/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInvestmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
